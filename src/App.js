e6cb8513170fb8220a74bbb82cf4e72ca6bb0428
“Yousef” <king_of_sword3@hotmail.com>
2023-12-26 21:39:07 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
-import Nav from './components/Nav';
-import Home from './components/Home';
-import TripsList from './components/TripsList';
-import TripDetail from './components/TripDetail';
+import "./App.css";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import TripsList from "./components/TripsList";
+import TripDetail from "./components/TripDetail";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div id="page-top">
       <Nav />
-      <Home />
-      <TripsList />
-      <TripDetail />
+      <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Children } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import TripsList from "./components/TripsList";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/tripItem",
+        element: <TripsList />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
